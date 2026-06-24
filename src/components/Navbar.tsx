9926c351b17553041ea5be9290de366fc86310cc
Vickiiv <virzivictoria@gmail.com>
2026-06-24 21:10:10 +0200

    navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,18 +10,47 @@ function Navbar() {
     navigate("/login");
   };
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/events">Events</Link>
-        <Link to="events/new">Event erstellen</Link>
+    <div className="text-xl py-2 flex justify-between fixed top-0 left-0 w-full backdrop-blur-2xl border-b shadow-md  bg-white/50 px-6">
+      <div>
+        <img className="py-2 px-2 w-22" src="/assets/logo.png" alt="logo" />
+      </div>
+      <nav className="flex justify-center items-center  gap-4">
+        <Link
+          className="font-bold hover:text-black text-gray-600 text-xl"
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="font-bold hover:text-black text-gray-600 text-xl"
+          to="/events"
+        >
+          Events
+        </Link>
+        <Link
+          className="font-bold hover:text-black text-gray-600 text-xl"
+          to="events/new"
+        >
+          Event erstellen
+        </Link>
       </nav>
       {token ? (
         <button onClick={handleLogout}>Logout</button>
       ) : (
-        <nav>
-          <Link to="/register">Registieren</Link>
-          <Link to="/login"> Login</Link>
+        <nav className="flex justify-center items-center  gap-2">
+          <Link
+            className="bg-blue-600 py-2 px-6 text-white rounded-2xl hover:bg-blue-900 font-bold cursor-pointer  "
+            to="/register"
+          >
+            Registieren
+          </Link>
+          <Link
+            className="bg-gray-600 py-2 px-6 text-white rounded-2xl hover:bg-gray-900 font-bold cursor-pointer  "
+            to="/login"
+          >
+            {" "}
+            Login
+          </Link>
         </nav>
       )}
     </div>
