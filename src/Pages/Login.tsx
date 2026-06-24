0bbefdc299de7a2fe2d73b16a4ef4af7bb7db850
Vickiiv <virzivictoria@gmail.com>
2026-06-24 21:34:14 +0200

    login styling

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -35,27 +35,51 @@ function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {error && <p>{error}</p>}
-      <h1>Login</h1>
-      <label htmlFor="email">E-mail</label>
-      <input
-        type="email"
-        id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+    <div className=" text-2xl m-50 flex items-center justify-center p-4">
+      <form
+        onSubmit={handleSubmit}
+        className=" flex flex-col bg-white rounded-2xl w-full max-w-xl shadow-2xl"
+      >
+        <h1 className="font-bold text-2xl py-2 text-center ">Login</h1>
+        {error && (
+          <p className="text-center text-red-800 font-bold text-xl">{error}</p>
+        )}
+        <label
+          className="mx-5 mb-1 font-bold text-2xl pl-2 pt-2"
+          htmlFor="email"
+        >
+          E-mail
+        </label>
+        <input
+          className="focus:outline-2 focus:outline-blue-700 bg-gray-50 border-2 rounded-xl px-2 py-3 mx-5 mb-6"
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-      <label htmlFor="password">Passwort</label>
-      <input
-        type="password"
-        id="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+        <label
+          className="mx-5 mb-1 font-bold text-2xl pl-2 pt-2"
+          htmlFor="password"
+        >
+          Passwort
+        </label>
+        <input
+          className="focus:outline-2 focus:outline-blue-700 bg-gray-50 border-2 rounded-xl px-2 py-3 mx-5 mb-6"
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-      <button type="submit">Anmelden</button>
-    </form>
+        <button
+          className="font-bold w-full sm:w-auto bg-blue-700 text-white py-2 mx-3 rounded-2xl mt-2 mb-6 hover:bg-blue-900 cursor-pointer"
+          type="submit"
+        >
+          Anmelden
+        </button>
+      </form>
+    </div>
   );
 }
 
