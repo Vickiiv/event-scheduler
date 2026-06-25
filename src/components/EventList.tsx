b413b414c679d06f87506eef11b0890a586de8ae
Vickiiv <virzivictoria@gmail.com>
2026-06-25 15:07:01 +0200

    styling improved

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -15,19 +15,19 @@ function EventList() {
     ladeEvents();
   }, []);
   return (
-    <div className="text-2xl  mt-30 flex flex-col p-6">
-      <h1 className="text-2xl font-bold mb-4">Events</h1>
+    <div className="mt-30 flex flex-col p-6">
+      <h1 className="text-3xl font-bold mb-4">Events</h1>
 
-      <div className="grid grid-cols-3 gap-4">
+      <div className="text-2xl  grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {events.map((event) => (
           <div
             key={event.id}
-            className="flex flex-col border rounded-lg p-6 shadow-xl"
+            className="flex flex-col border-2 border-gray-200 rounded-xl bg-white p-6 shadow-md hover:shadow-xl transition"
           >
             <h2 className="font-semibold">{event.title}</h2>
             <p className="text-gray-500">{event.location}</p>
             <Link
-              className="bg-blue-600 p-2 mt-20 text-white rounded-2xl text-center cursor-pointer  hover:bg-blue-800"
+              className="bg-blue-600 p-2 justify-between text-white rounded-lg text-center cursor-pointer  hover:bg-blue-800"
               to={`/events/${event.id}`}
             >
               Details
