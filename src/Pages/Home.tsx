047ef52bd0a46e95ed5f34d6255bfb593bc746a7
Vickiiv <virzivictoria@gmail.com>
2026-06-29 14:23:35 +0200

    add responsive design

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,23 +4,22 @@ function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="relative h-screen w-full">
-      <div className="mt-25">
-        <img
-          className="absolute inset-0 h-full w-full object-cover"
-          src="/assets/header.jpg"
-        ></img>
-      </div>
-      <div className="absolute inset-0 flex items-center justify-center gap-10">
+    <div className="relative  min-h-dvh w-full overflow-hidden">
+      <img
+        className="absolute inset-0 h-full w-full object-cover"
+        src="/assets/header.jpg"
+      ></img>
+
+      <div className="absolute inset-0 flex flex-col sm:flex-row items-center justify-center gap-4 sm:gap-10 px-6">
         <button
           onClick={() => navigate("/events")}
-          className="bg-blue-700/80 hover:bg-blue-800 text-white font-semibold py-4 px-8 text-xl rounded-2xl shadow-lg transition"
+          className="w-full max-w-xs sm:w-auto bg-blue-700/80 hover:bg-blue-800 text-white font-semibold py-3 px-6 text-lg sm:py-4 sm:px-8 sm:text-xl rounded-2xl shadow-lg transition"
         >
           Events entdecken
         </button>
         <button
           onClick={() => navigate("/events/new")}
-          className="bg-blue-700/80 hover:bg-blue-800 text-white font-semibold py-4 px-8 text-xl rounded-2xl shadow-lg transition"
+          className="w-full max-w-xs sm:w-auto bg-blue-700/80 hover:bg-blue-800 text-white font-semibold py-3 px-6 text-lg sm:py-4 sm:px-8 sm:text-xl rounded-2xl shadow-lg transition"
         >
           Event erstellen
         </button>
